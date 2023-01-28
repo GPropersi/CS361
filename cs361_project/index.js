d9be8f9ebd84218508bac7ca2bea078c7cf6ce1e
Giovanni Propersi <giovannigp@gmail.com>
2023-01-27 19:51:10 -0600

    Settings options complete

--- cs361_project/index.js
+++ cs361_project/index.js
@@ -5,7 +5,6 @@ import chalkAnimation from "chalk-animation";
 import gradient from "gradient-string";
 import align_text from "align-text";
 import center_align from "center-align";
-// import { promises as fs } from 'fs';
 import fs from 'fs';
 import { createSpinner } from "nanospinner";
 
@@ -17,6 +16,10 @@ const DB_FOLDER = "./db_files";
 const PIPE_TO_API = "./listener_for_random_word.json";
 const ui = new inquirer.ui.BottomBar();
 const SETTINGS_TEXT = "Settings Menu - Use Arrow Keys to Select and Modify Your Settings";
+const MIN_WORD_LENGTH = 3;
+const MAX_WORD_LENGTH = 15;
+const MAIN_MENU_LINES = '\n\n\n\n\n\n\n\n\n\n\n';
+
 let bottomTitleText;
 let current_username;
 let menuChoice;
@@ -47,10 +50,12 @@ let user = {
         13: {W: 0, L: 0},
         14: {W: 0, L: 0},
         15: {W: 0, L: 0},
-    }
+    },
+    words_played: [],
 }
 
 function titleBlockMain() {
+    console.clear()
     console.log(gradient.pastel.multiline(figlet.textSync(`Guess The Word! !\n`, {
         verticalLayout: 'full',
         horizontalLayout: 'full',
@@ -82,7 +87,7 @@ async function askIfGuest() {
         choices: [
         'Login/Register with Username',
         'Play as Guest -- Jump Right In!',
-        'Exit',
+        'Exit'
         ],
     });
 
@@ -172,13 +177,12 @@ async function showLoadingSpinner(updateText) {
 async function showBottomBar(barText) {
     ui.updateBottomBar(barText);
 }
-const mainMenuLines = '\n\n\n\n\n\n\n\n\n\n\n';
 
 async function mainMenuGuest() {
     const answers = await inquirer.prompt({
         name: 'main_menu_option',
         type: 'list',
-        prefix: mainMenuLines,
+        prefix: MAIN_MENU_LINES,
         message: 'Select an Option Below Using the Return Key:',
         choices: [
         'Guess A Word!',
@@ -194,13 +198,25 @@ async function mainMenu() {
     const answers = await inquirer.prompt({
         name: 'main_menu_option',
         type: 'list',
-        prefix: mainMenuLines,
+        prefix: MAIN_MENU_LINES,
         message: 'Select an Option Below Using the Return Key:',
         choices: [
-        'Guess A Word!',
-        'Settings',
-        'Records',
-        'Save and Exit'
+        {
+            name: 'Guess A Word!',
+            short: `Let's Play!`
+        },
+        {
+            name: 'Settings',
+            short: `Let's Edit Your Settings!`
+        },
+        {
+            name: 'Records',
+            short: `What's Too Much Play Time?`
+        },
+        {
+            name: 'Save and Exit',
+            short: `Come Again!`
+        },
         ],
     });
 
@@ -211,21 +227,26 @@ async function handleMenu(menuOption) {
     switch (menuOption) {
         case 'Settings': {
             await showLoadingSpinner('Loading Settings');
+            console.clear()
             await titleBlock(SETTINGS_TEXT, settings);
+            break;
         }
         case 'Exit': {
             console.clear();
             process.exit(0);
+            break;
         }
         case 'Save and Exit': {
             await saveUserData();
             await showLoadingSpinner('Saving User Data');
             console.clear();
             process.exit(0);
+            break;
         }
         case 'Guess A Word!': {
             await requestWord();
             await playGame();
+            break;
         }
     }
 }
@@ -234,19 +255,196 @@ async function settings() {
     const settingsAnswer = await inquirer.prompt({
         name: 'settings_selection',
         type: 'list',
+        prefix: MAIN_MENU_LINES,
         message: 'Select an Option Below Using the Arrow Keys and Return Key:',
         choices: [
-        'Word Length',
-        'Word Hints',
-        'Skip Instructions on New Game',
-        'Okay to Repeat Words',
-        'Save and Return to Menu'
-        ],
+        {
+            name: 'Word Length',
+            short: `Guess a New Word Length!`
+        },
+        {
+            name: 'Word Hints',
+            short: `Make the Game Harder or Easier!`
+        },
+        {
+            name: 'Skip Instructions on New Game',
+            short: `Don't Need These Anymore!`
+        },
+        {
+            name: 'Okay to Repeat Words',
+            short: `Groundhog Day, Let's Go!`
+        },
+        {
+            name: 'Save and Return to Menu',
+            short: `Good Options`         
+        }],
     });
 
-    if (settingsAnswer.settings_selection === 'Save and Return to Menu') {
-        await titleBlock(bottomTitleText, menuChoice)
+    switch (settingsAnswer.settings_selection) {
+        case 'Word Length': {
+            await showLoadingSpinner(`Getting Current Word Length`);
+            await titleBlock(`Choose Your Word Length\nCurrent Word Length: ${user.settings.word_length}`, editWordLength)
+            break;
+        }
+        case 'Word Hints': {
+            await showLoadingSpinner('Getting Current Number of Word Hints');
+            await titleBlock(`Choose Uour Number of Hints\nCurrent Number of Hints: ${user.settings.hints}`, editWordHints)
+            break;
+        }
+        case 'Skip Instructions on New Game': {
+            await showLoadingSpinner('Getting Current Selection for Skipping Instructions');
+            await titleBlock('Want to Skip the Instructions?', editSkipInstructions)
+            break;
+        }
+        case 'Okay to Repeat Words': {
+            await showLoadingSpinner('Getting Current Selection for Repeating Words');
+            await titleBlock('Want to Allow Words to Repeat?', editRepeatWords)
+            break;
+        }
+        case 'Save and Return to Menu': {
+            await showLoadingSpinner('Saving Settings...');
+            if (current_username !== GUEST) {
+                await saveUserData();
+            }
+            updateBottomTitleText()
+            break;
+        }
     }
+}
+
+async function editWordLength() {
+    const currentWordLength = user.settings.word_length;
+    let newChoices = [];
+
+    for (let i = MIN_WORD_LENGTH; i <= MAX_WORD_LENGTH; i++) {
+        newChoices.push({
+            name: `${i}`,
+            short: `You want words ${i} characters long`
+        })
+    }
+
+    const wordLengthAnswer = await inquirer.prompt({
+        name: 'wordLengthSelection',
+        type: 'list',
+        prefix: MAIN_MENU_LINES,
+        default: `${currentWordLength}`,
+        message: 'Select an Option Below Using the Arrow Keys and Return Key:',
+        choices: newChoices
+    });
+
+    user.settings.word_length = wordLengthAnswer.wordLengthSelection
+    if (user.settings.hints >= user.settings.word_length - 1) {
+        user.settings.hints = user.settings.word_length - 2
+    }
+    await showLoadingSpinner('Returning to Settings...');
+    await titleBlock(SETTINGS_TEXT, settings);
+}
+
+async function editWordHints() {
+    const currentWordHints = user.settings.hints;
+    const currentWordLength = user.settings.word_length;
+    let newChoices = [];
+
+    for (let i = 0; i < currentWordLength - 1; i++) {
+        newChoices.push({
+            name: `${i}`,
+            value: `${i}`,
+            short: `You want ${i} hints`
+        })
+    }
+
+    const wordHintsAnswer = await inquirer.prompt({
+        name: 'wordHintsSelection',
+        type: 'list',
+        prefix: MAIN_MENU_LINES,
+        default: `${currentWordHints}`,
+        message: 'Select an Option Below Using the Arrow Keys and Return Key:',
+        choices: newChoices
+    });
+
+    user.settings.hints = wordHintsAnswer.wordHintsSelection
+    await showLoadingSpinner('Returning to Settings...');
+    await titleBlock(SETTINGS_TEXT, settings);
+}
+
+async function editSkipInstructions() {
+    const currentSelection = user.settings.show_instructions;
+    
+    let skipInstructionsOption = {
+        name: `Yes`,
+        short: `No More Instructions For You!`
+    }
+    let doNotSkipInstructionsOption = {
+        name: `No`,
+        short: `All of the Instructions!`
+    }
+    let defaultOption;
+
+    if (currentSelection) {
+        defaultOption = `No`
+    } else {
+        defaultOption = `Yes`
+    }
+
+    let newChoices = [skipInstructionsOption, doNotSkipInstructionsOption];
+
+    const skipInstructionsAnswer = await inquirer.prompt({
+        name: 'skipInstructionsSelection',
+        type: 'list',
+        prefix: MAIN_MENU_LINES,
+        default: defaultOption,
+        message: 'Want to Skip the Instructions Before Each Game?',
+        choices: newChoices
+    });
+
+    if (skipInstructionsAnswer.skipInstructionsSelection === `Yes`) {
+        user.settings.show_instructions = false
+    } else {
+        user.settings.show_instructions = true
+    }
+
+    await showLoadingSpinner('Returning to Settings...');
+    await titleBlock(SETTINGS_TEXT, settings);
+}
+
+async function editRepeatWords() {
+    const currentSelection = user.settings.allow_repeats;
+    
+    let allowRepeatsOption = {
+        name: `Yes`,
+        short: `Deja Vu?!`
+    }
+    let doNotAllowRepeatsOption = {
+        name: `No`,
+        short: `Let's Get Fresh Words in Here!`
+    }
+    let defaultOption;
+
+    if (currentSelection) {
+        defaultOption = `Yes`
+    } else {
+        defaultOption = `No`
+    }
+
+    let newChoices = [allowRepeatsOption, doNotAllowRepeatsOption];
+
+    const editRepeatsAnswer = await inquirer.prompt({
+        name: 'editRepeatsSelection',
+        type: 'list',
+        prefix: MAIN_MENU_LINES,
+        default: defaultOption,
+        message: 'Want to Allow Words to Repeat?',
+        choices: newChoices
+    });
+
+    if (editRepeatsAnswer.editRepeatsSelection === `Yes`) {
+        user.settings.allow_repeats = true
+    } else {
+        user.settings.allow_repeats = false
+    }
+
+    await showLoadingSpinner('Returning to Settings...');
+    await titleBlock(SETTINGS_TEXT, settings);
 }
 
 async function saveUserData(){
@@ -314,6 +512,17 @@ async function makeFilesIfDoNotExist() {
     }
 }
 
+function updateBottomTitleText() {
+    if (current_username === GUEST) {
+        bottomTitleText = `Playing As: ${user.username}\n` +
+            `Note: Your Win/Loss Record and User Settings Will not be Saved\n` +
+            `Wins: ${user.wins}    Losses: ${user.losses}    Current Word Length: ${user.settings.word_length}    Hints Available: ${user.settings.hints}`
+    } else {
+        bottomTitleText = `Playing As: ${user.username}\n` +
+            `Total Wins: ${user.wins}    Total Losses: ${user.losses}    Current Word Length: ${user.settings.word_length}    Hints Available: ${user.settings.hints}`
+    }
+}
+
 async function checkIfFileExists (path) {  
     try {
         await fs.promises.access(path);
@@ -324,6 +533,7 @@ async function checkIfFileExists (path) {
 }
   
 
+// Top line functions to begin CLI app
 await makeFolderIfDoNotExist();
 await makeFilesIfDoNotExist();
 
@@ -331,16 +541,12 @@ await titleBlock('A Node.js CLI Word Guessing Game - Made By Giovanni Propersi',
 
 if (current_username === GUEST) {
     menuChoice = mainMenuGuest;
-    bottomTitleText = `Playing As: ${user.username}\n` +
-        `Note: Your Win/Loss Record and User Settings Will not be Saved\n` +
-        `Wins: ${user.wins}    Losses: ${user.losses}    Current Word Length: ${user.settings.word_length}`
 } else {
     menuChoice = mainMenu;
-    bottomTitleText = `Playing As: ${user.username}\n` +
-        `Total Wins: ${user.wins}    Total Losses: ${user.losses}    Current Word Length: ${user.settings.word_length}`
 }
 
 while (letsPlay) {
+    updateBottomTitleText()
     await titleBlock(bottomTitleText, menuChoice);
     await handleMenu(menuSelection);
 }
